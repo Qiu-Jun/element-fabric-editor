22e8c882aefe747f4835a8697a6befe79f3d17a0
June <1601745371@qq.com>
2024-07-26 12:23:14 +0800

    chore: 🚀 同步代码

--- src/hooks/select.ts
+++ src/hooks/select.ts
@@ -1,26 +1,33 @@
-import Editor, { EventType } from '@/lib/core'
-import { useI18n } from 'vue-i18n'
+/*
+ * @Author: June
+ * @Description: 
+ * @Date: 2024-07-25 14:39:41
+ * @LastEditTime: 2024-07-26 12:15:29
+ * @LastEditors: June
+ * @FilePath: \element-fabric-editor\src\hooks\select.ts
+ */
+import Editor from '@/lib/core';
+import { useI18n } from 'vue-i18n';
+import { Selector } from './useSelectListen';
 
-const { SelectMode } = EventType
-
-interface Selector {
-  mSelectMode: (typeof SelectMode)[keyof typeof SelectMode]
-  mSelectOneType: string | undefined
-  mSelectId: string | undefined
-  mSelectIds: (string | undefined)[]
-  mSelectActive: unknown[]
-}
+// interface Selector {
+//   mSelectMode: (typeof SelectMode)[keyof typeof SelectMode];
+//   mSelectOneType: string | undefined;
+//   mSelectId: string | undefined;
+//   mSelectIds: (string | undefined)[];
+//   mSelectActive: unknown[];
+// }
 
 export default function useSelect() {
-  const fabric = inject('fabric')
-  const { t } = useI18n()
-  const canvasEditor = inject('canvasEditor') as Editor
-  const mixinState = inject('mixinState') as Selector
+  const fabric = inject('fabric');
+  const { t } = useI18n();
+  const canvasEditor = inject('canvasEditor') as Editor;
+  const mixinState = inject('mixinState') as Selector;
 
   return {
     fabric,
     canvasEditor,
     mixinState,
-    t
-  }
+    t,
+  };
 }
--- src/hooks/useSelectListen.ts
+++ src/hooks/useSelectListen.ts
@@ -1,14 +1,22 @@
-import Editor, { EventType } from '@/lib/core'
-import { get } from 'lodash-es'
+/*
+ * @Descripttion: useSelectListen
+ * @version:
+ * @Author: wuchenguang1998
+ * @Date: 2024-05-04 14:36:49
+ * @LastEditors: June
+ * @LastEditTime: 2024-07-26 12:16:35
+ */
+import Editor, { EventType } from '@/lib/core';
+import { get } from 'lodash-es';
 
-const { SelectEvent, SelectMode } = EventType
+const { SelectEvent, SelectMode } = EventType;
 
-interface Selector {
-  mSelectMode: (typeof SelectMode)[keyof typeof SelectMode]
-  mSelectOneType: string | undefined
-  mSelectId: string | undefined
-  mSelectIds: (string | undefined)[]
-  mSelectActive: unknown[]
+export interface Selector {
+  mSelectMode: (typeof SelectMode)[keyof typeof SelectMode];
+  mSelectOneType: string | undefined;
+  mSelectId: string | undefined;
+  mSelectIds: (string | undefined)[];
+  mSelectActive: unknown[];
 }
 
 export default function useSelectListen(canvasEditor: Editor) {
@@ -17,52 +25,52 @@ export default function useSelectListen(canvasEditor: Editor) {
     mSelectOneType: '',
     mSelectId: '', // 选择id
     mSelectIds: [], // 选择id
-    mSelectActive: []
-  })
+    mSelectActive: [],
+  });
 
   const selectOne = (e: [fabric.Object]) => {
-    state.mSelectMode = SelectMode.ONE
-    state.mSelectActive = e[0]
+    state.mSelectMode = SelectMode.ONE;
+    state.mSelectActive = e;
     if (e[0] && get(e[0], 'clip')) {
-      selectCancel()
+      selectCancel();
       // state.mSelectId = get(e[0], 'targetId');
       // state.mSelectOneType = get(e[0], 'targetType');
       // state.mSelectIds = e.map((item) => get(item, 'targetId'));
-      return
+      return;
     }
     if (e[0]) {
-      state.mSelectId = e[0].id
-      state.mSelectOneType = e[0].type
-      state.mSelectIds = e.map((item) => item.id)
+      state.mSelectId = e[0].id;
+      state.mSelectOneType = e[0].type;
+      state.mSelectIds = e.map((item) => item.id);
     }
-  }
+  };
 
   const selectMulti = (e: fabric.Object[]) => {
-    state.mSelectMode = SelectMode.MULTI
-    state.mSelectId = ''
-    state.mSelectIds = e.map((item) => item.id)
-  }
+    state.mSelectMode = SelectMode.MULTI;
+    state.mSelectId = '';
+    state.mSelectIds = e.map((item) => item.id);
+  };
 
   const selectCancel = () => {
-    state.mSelectId = ''
-    state.mSelectIds = []
-    state.mSelectMode = SelectMode.EMPTY
-    state.mSelectOneType = ''
-  }
+    state.mSelectId = '';
+    state.mSelectIds = [];
+    state.mSelectMode = SelectMode.EMPTY;
+    state.mSelectOneType = '';
+  };
 
   onMounted(() => {
-    canvasEditor.on(SelectEvent.ONE, selectOne)
-    canvasEditor.on(SelectEvent.MULTI, selectMulti)
-    canvasEditor.on(SelectEvent.CANCEL, selectCancel)
-  })
+    canvasEditor.on(SelectEvent.ONE, selectOne);
+    canvasEditor.on(SelectEvent.MULTI, selectMulti);
+    canvasEditor.on(SelectEvent.CANCEL, selectCancel);
+  });
 
   onBeforeMount(() => {
-    canvasEditor.off(SelectEvent.ONE, selectOne)
-    canvasEditor.off(SelectEvent.MULTI, selectMulti)
-    canvasEditor.off(SelectEvent.CANCEL, selectCancel)
-  })
+    canvasEditor.off(SelectEvent.ONE, selectOne);
+    canvasEditor.off(SelectEvent.MULTI, selectMulti);
+    canvasEditor.off(SelectEvent.CANCEL, selectCancel);
+  });
 
   return {
-    mixinState: state
-  }
+    mixinState: state,
+  };
 }
