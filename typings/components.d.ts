6f3f8f09b843bd92c42e23c7599a0c677bf713c6
June <1601745371@qq.com>
2024-08-19 13:27:18 +0800

    chore: 🚀 移除mitt依赖

--- typings/components.d.ts
+++ typings/components.d.ts
@@ -32,8 +32,6 @@ declare module 'vue' {
     ElBreadcrumbItem: typeof import('element-plus/es')['ElBreadcrumbItem']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElButtonGroup: typeof import('element-plus/es')['ElButtonGroup']
-    ElCarousel: typeof import('element-plus/es')['ElCarousel']
-    ElCarouselItem: typeof import('element-plus/es')['ElCarouselItem']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElCol: typeof import('element-plus/es')['ElCol']
     ElCollapse: typeof import('element-plus/es')['ElCollapse']
