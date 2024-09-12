c0ceddcae63b605fafaaf2eece38c1a4e0a3a119
June <1601745371@qq.com>
2024-09-12 10:39:57 +0800

    chore: 🚀 uno优化

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,16 @@
+/*
+ * @Author: June
+ * @Description:
+ * @Date: 2024-08-19 12:53:30
+ * @LastEditTime: 2024-09-12 10:39:32
+ * @LastEditors: June
+ * @FilePath: \element-fabric-editor\uno.config.ts
+ */
 import { defineConfig, presetAttributify, presetUno } from 'unocss'
+import transformerCompileClass from '@unocss/transformer-compile-class' // 将一组类编译为一个类
 
 export default defineConfig({
+  transformers: [transformerCompileClass()],
   presets: [presetUno(), presetAttributify()],
   shortcuts: [
     { 'wh-full': 'w-full h-full' },
