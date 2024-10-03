6cc616431bae60566c73dc7700e7e6d50fd240b9
June <1601745371@qq.com>
2024-10-03 11:18:02 +0800

    chore: 🚀 优化unocss配置

--- src/store/modules/template.ts
+++ src/store/modules/template.ts
@@ -1,7 +1,19 @@
+/*
+ * @Author: June
+ * @Description:
+ * @Date: 2024-09-30 17:20:34
+ * @LastEditTime: 2024-10-03 11:14:55
+ * @LastEditors: June
+ * @FilePath: \element-fabric-editor\src\store\modules\template.ts
+ */
 import { defineStore } from 'pinia'
 import store from '@/store'
+import { fabric } from 'fabric'
 
-interface IState {}
+interface IState {
+  curTemplate: fabric.Object | null
+  curTempIdx: number
+}
 export const useTemplateStore = defineStore({
   id: 'Template',
   state: (): IState => ({}),
--- uno.config.ts
+++ uno.config.ts
@@ -2,17 +2,24 @@
  * @Author: June
  * @Description:
  * @Date: 2024-08-19 12:53:30
- * @LastEditTime: 2024-09-12 10:39:32
+ * @LastEditTime: 2024-10-03 11:17:28
  * @LastEditors: June
  * @FilePath: \element-fabric-editor\uno.config.ts
  */
-import { defineConfig, presetAttributify, presetUno } from 'unocss'
+import {
+  defineConfig,
+  presetAttributify,
+  presetUno,
+  transformerDirectives
+} from 'unocss'
 import transformerCompileClass from '@unocss/transformer-compile-class' // 将一组类编译为一个类
 
 export default defineConfig({
+  transformers: [transformerDirectives(), transformerCompileClass()],
   presets: [presetUno(), presetAttributify()],
   shortcuts: [
     { 'wh-full': 'w-full h-full' },
-    { 'f-center': 'flex justify-center items-center' }
+    { 'f-center': 'flex justify-center items-center' },
+    { 'text-over-1': 'overflow-hidden text-ellipsis whitespace-nowrap' }
   ]
 })
